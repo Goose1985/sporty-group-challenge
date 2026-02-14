0808187b3b0ce18e9853ef087c3dea615f4e9b57
Gustavo Romero <gustavo.romero@mebebot.com>
2026-02-14 02:09:29 -0300

    feat(app): add Spring Boot main application class

---
+++ src/main/java/com/sporty/group/SportyGroupChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.sporty.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyGroupChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportyGroupChallengeApplication.class, args);
+    }
+}
